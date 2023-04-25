bd28365eb367366551132faac4bc1f5e357ddc32
Harsimran Kaur <81485814+HARSIMRANNN@users.noreply.github.com>
2023-04-25 10:23:34 +0530

    Add files via upload

---
+++ task28.js
@@ -0,0 +1,16 @@
+// query selector
+// console.log(document.querySelector('.r'))
+
+// getelementsbyclassname
+// let b = document.getElementsByClassName('r')
+// for(let i=0;i<=2;i++){
+//     b[i].style.color="pink"
+// }
+
+//queryselectorAll
+// console.log(document.querySelectorAll('.r'))
+
+//getElementByTagName
+// console.log(document.getElementsByTagName('img'))
+
+console.dir(document.getElementsByTagName('img'))
